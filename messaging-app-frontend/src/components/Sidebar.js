80acc648fe2f3121915aa03a2eb09a7840fcad2a
xaviSalazar <xavicoel@gmail.com>
2022-08-04 11:31:53 +0200

    added sidebar component

---
+++ messaging-app-frontend/src/components/Sidebar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './Sidebar.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
+
+import { SearchOutlined } from "@material-ui/icons";
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                 <Avatar src="https://pbs.twimg.com/profile_images/1020939891457241088/fcbu814K_400x400.jpg"/>
+               <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+            <div className="sidebar__chats"></div>
+        </div>
+    )
+}
+
+export default Sidebar;
