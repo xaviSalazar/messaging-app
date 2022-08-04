e180d4ddd4ac1107c16740a507c3a5bdc1de8576
xaviSalazar <xavicoel@gmail.com>
2022-08-04 14:17:39 +0200

    added chat component

---
+++ messaging-app-frontend/src/components/Chat.js
@@ -0,0 +1,72 @@
+import React, {useEffect, useState } from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import './Chat.css';
+import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon } from '@material-ui/icons';
+import MicIcon from '@material-ui/icons/Mic'
+
+const Chat = () => {
+    const [seed, setSeed] = useState("");
+
+    useEffect(() => { 
+        setSeed(Math.floor(Math.random()*5000))
+    }, [])
+
+    return (
+        <div className='chat'>
+            <div className='chat__header'>
+                <Avatar src = {`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
+                <div className='chat__headerInfo'>
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className='chat__headerRight'>
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className='chat__body'>
+                <p className='chat__message'>
+                    <span className='chat__name'>victor</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className='chat__message chat__receiver'>
+                    <span className='chat__name'>Parag</span>
+                    This is a message back
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className='chat__message'>
+                    <span className='chat__name'>victor</span>
+                    This is another message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+            <div className='chat__footer'>
+                <InsertEmoticon />
+                <form>
+                    <input  
+                        placeholder='Type a message'
+                        type='text'
+                    />
+                    <button type="submit">Send Message</button>
+                </form>
+                <MicIcon/>
+            </div>
+        </div>
+    )
+}
+
+export default Chat;
