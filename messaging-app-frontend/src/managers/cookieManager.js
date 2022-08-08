d0697603411fe50b947c3d03b792f6f7185e1d55
xaviSalazar <xavicoel@gmail.com>
2022-08-08 09:22:08 +0200

    feat: cookie manager

---
+++ messaging-app-frontend/src/managers/cookieManager.js
@@ -0,0 +1,16 @@
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
+const setUserInfo = (userInfo) => {
+    cookies.set('userInfo', JSON.stringify(userInfo), {path: '/'});
+}
+
+const getUserInfo = () => {
+    return cookies.get('userInfo');
+}
+
+export const cookieManager = {
+    setUserInfo,
+    getUserInfo
+}
