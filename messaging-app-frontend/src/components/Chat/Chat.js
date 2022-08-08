41bc667fc722c68d98f5a2347f6d64277ae66472
xaviSalazar <xavicoel@gmail.com>
2022-08-08 11:20:39 +0200

    feat: handlesubmit to preventdefault

--- messaging-app-frontend/src/components/Chat/Chat.js
+++ messaging-app-frontend/src/components/Chat/Chat.js
@@ -5,14 +5,27 @@ import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon } from '@material-
 import MicIcon from '@material-ui/icons/Mic'
 import './utils/Messagebox'
 import Messagebox from './utils/Messagebox';
-import { messagesList } from '../Sidebar/Mockdata/Mockdata'
+//import { messagesList } from '../Sidebar/Mockdata/Mockdata'
 // import Picker from "emoji-picker-react";
 
 const Chat = (props) => {
 
     const { selectedChat } = props;
+    const [messageList, setMessageList] = useState([]);
+    const [message, SetMessage] = useState("");
 
     const [seed, setSeed] = useState("");
+
+    const handleSubmit = (e) => {
+        console.log("handle submite")
+        SetMessage("");
+        e.preventDefault(); 
+    }
+
+    const onMessageTextChanged = (typedText) => {
+        SetMessage(typedText);
+        console.log(typedText);
+    }
 
     useEffect(() => { 
         setSeed(Math.floor(Math.random()*5000))
@@ -42,7 +55,7 @@ const Chat = (props) => {
             <div className='chat__body'>
 
                 {
-                    messagesList.map((userDataMessage, index) => (
+                    messageList.map((userDataMessage, index) => (
                         <Messagebox key = {index} userDataMessage = {userDataMessage}/>
                     ))
                 }
@@ -51,10 +64,12 @@ const Chat = (props) => {
             <div className='chat__footer'>
                 <InsertEmoticon />
                 {/* <Picker onEmojiClick={onEmojiClick} /> */}
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input  
                         placeholder='Type a message'
                         type='text'
+                        value = {message}
+                        onChange={(e) => onMessageTextChanged(e.target.value)}
                     />
                     <button type="submit">Send Message</button>
                 </form>
