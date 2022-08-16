633995c0ac616207af0fdef44b520397eccda59c
xaviSalazar <xavicoel@gmail.com>
2022-08-16 21:52:05 +0200

    refactor: move lines of code outside function

--- src/components/Sidebar/SidebarChat/SidebarChat.js
+++ src/components/Sidebar/SidebarChat/SidebarChat.js
@@ -5,6 +5,25 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from "react-redux";
 import { getMessagesFromChannel } from '../../../redux/GetMessages/Actions'
 
+const filterMessages = (userMessages, setShowInfo, userData) => {
+    
+    let veamos = userData
+
+    var messagesToFilter = userMessages.filter( function(msg) {
+        return msg.from === userData.phoneNumber
+    });
+
+    var objet = messagesToFilter.pop();
+
+    if(objet) {
+    console.log(objet.message)
+    veamos.lastMessage = objet.message
+
+    const temporaire = {...veamos}
+    setShowInfo(temporaire)
+    }
+}
+
 const SidebarChat = (props) => {
 
     const dispatch = useDispatch();
@@ -16,29 +35,12 @@ const SidebarChat = (props) => {
 
     const setMessageAndChat = async () => {
         setChat(userData);
-        console.log(userData)
+        // console.log(userData)
         dispatch(getMessagesFromChannel(userData.phoneNumber))
     }
 
     useEffect(() => {
-
-        console.log(`useEffect sidebarchat js`)
-        let veamos = userData
-        console.log(userMessages)
-        var messagesToFilter = userMessages.filter( function(msg) {
-            return msg.from === userData.phoneNumber
-        });
-
-        var objet = messagesToFilter.pop();
-
-        if(objet) {
-        console.log(objet.message)
-        veamos.lastMessage = objet.message
-
-        const temporaire = {...veamos}
-        setShowInfo(temporaire)
-        }
-
+        filterMessages(userMessages, setShowInfo, userData)
     }, [userMessages, userData])
 
     return (
