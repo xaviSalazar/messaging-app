9374cd057a8ea6ea7abe9b8c48c45418ad0777ee
xaviSalazar <xavicoel@gmail.com>
2022-08-18 20:31:23 +0200

    refactor: forgot localhost to comment

--- src/managers/socketioManager.js
+++ src/managers/socketioManager.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client"
 
-const URL = "http://localhost:3001";
+//const URL = "http://localhost:3001";
 
-//const URL = "https://whatsapp-cloud-backend.herokuapp.com";
+const URL = "https://whatsapp-cloud-backend.herokuapp.com";
 
 const socket = io(URL, {autoConnect: false});
 
