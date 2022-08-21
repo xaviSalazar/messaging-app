60f454ab6796cdb8c4725f35eab886b4b86a5d15
xaviSalazar <xavicoel@gmail.com>
2022-08-21 15:53:23 +0200

    to check tokens

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from 'react-redux';
 import { configPhoneNumber } from "../redux/ConfigToken/Actions";
 import EditIcon from '@material-ui/icons/Edit';
 import {  IconButton } from '@material-ui/core';
 import { Button } from "@material-ui/core";
+import { PersonalVideo } from "@material-ui/icons";
             
 
 const Settings = () => {
 
     const [phoneNumber, setPhoneNumber] = useState('')
     const [token, setToken] = useState('')
+    const [phoneId, setPhoneId] = useState('')
     // local state to input
-    const [isDisabled, setIsDisabled] = useState({ phone_num: true, what_token: true });
+    const [isDisabled, setIsDisabled] = useState({ phone_num: true, what_token: true, phone_id: true});
+    const [inputValues, setInputValues] = useState({ phoneNumber: "inserte numero", what_token: "inserte token", phone_id: "inserte phone id" });
+
+    useEffect(() => { 
+
+        const itemStr = localStorage.getItem("whatsapp_app")
+        if(!itemStr) 
+        {
+            return;
+        }
+        const item = JSON.parse(itemStr);
+       // setInputValues({ phoneNumber: item.phoneNumber, what_token: item.token })
+
+    }, [])
 
     const handleClick = ( value ) => {
         
@@ -23,40 +38,41 @@ const Settings = () => {
             let lock = !isDisabled.what_token
             setIsDisabled({...isDisabled, what_token: lock})
         }
+
+        if(value === 'phone_id') {
+            let lock = !isDisabled.phone_id
+            setIsDisabled({...isDisabled, phone_id: lock})
+        }
         console.log(value)
        
-
     }
 
-    const dispatch = useDispatch();
+
     const setConfigurations = () => {
-        
+
         if( phoneNumber && token)
         {
             console.log('clicked')
+
             const config = {
                 phoneNumber : phoneNumber,
-                token : token
+                token : token,
+                phoneId : phoneId
             }
 
-        dispatch(configPhoneNumber(config));
+            localStorage.setItem("whatsapp_app", JSON.stringify(config))
+            alert("saved data")
+
         }
     }
 
-    const inputPhoneNum = (typedText) =>{
-        setPhoneNumber(typedText);
-    } 
-
-    const inputToken = (typedText) =>{
-        setToken(typedText)
-    } 
 
     return (
         <div className="settings-app">
             <label >
                 Insert phone number:
             </label>
-            <input type="text" id="phone" disabled = {isDisabled.phone_num} onChange={(e) => inputPhoneNum(e.target.value)}/>
+            <input type="text" value = {inputValues.phoneNumber} disabled = {isDisabled.phone_num} onChange={(e) => setPhoneNumber(e.target.value)}/>
             <IconButton onClick = {() => handleClick('phone_num')}>
              <EditIcon/>
             </IconButton>
@@ -64,16 +80,24 @@ const Settings = () => {
             <label >
                 Insert secret token:
             </label>
-            <input type="text" disabled = {isDisabled.what_token} onChange={(e) => inputPhoneNum(e.target.value)}/>
+            <input type="text" value = {inputValues.what_token} disabled = {isDisabled.what_token} onChange={(e) => setToken(e.target.value)}/>
             <IconButton onClick = {() => handleClick('what_token')}>
              <EditIcon/>
             </IconButton>
             <br/>
+            <label >
+                Insert number id:
+            </label>
+            <input type="text" value = {inputValues.phone_id} disabled = {isDisabled.phone_id} onChange={(e) => setPhoneId(e.target.value)}/>
+            <IconButton onClick = {() => handleClick('phone_id')}>
+             <EditIcon/>
+            </IconButton>
+            <br/>
+            <br/>
+            <br/>
             <Button 
                 variant="contained"
-                onClick={() => {
-                    alert('clicked');
-                  }}
+                onClick={setConfigurations}
             >
                 VALIDATE TOKENS
             </Button>
